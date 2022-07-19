4f808dbcf7c1437bdb3bfd9a3bd5dde8239e692f
Bimal Poudel <anytizer@users.noreply.github.com>
2022-07-18 20:31:21 -0600

    Create lyrics.cs

---
+++ src/timetagger/lyrics.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace timetagger
+{
+    public class lyrics
+    {
+        public string time { get; set; } = "00:00:00.00";
+        public string line { get; set; } = "";
+    }
+}
